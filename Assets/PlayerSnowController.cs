d0225ccd1e6ff3cfb2a5d74547f3ed69bb4c69ff
Ludo-sys21 <ludovico.angelini@gmail.com>
2025-03-05 14:00:29 +0100

    Minor fixes, comments, white snow
    -Colliders
    -Tarmac tweaks
    -Snow particles changed to white
    -Updated some code comments

--- Assets/PlayerSnowController.cs
+++ Assets/PlayerSnowController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerSnowController : MonoBehaviour
 {
-    // Toggles the active state of the fog effect.
+    //Toggles the active state of the snow effect around the player.
     public void SetVisible(bool visible)
     {
         gameObject.SetActive(visible);
--- Assets/Scripts/GameStates/GlobalSnowController.cs
+++ Assets/Scripts/GameStates/GlobalSnowController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class GlobalSnowController : MonoBehaviour
 {
-    // Toggles the active state of the snow effect.
+    //Toggles the active state of the global snow effect.
     public void SetVisible(bool visible)
     {
         gameObject.SetActive(visible);
